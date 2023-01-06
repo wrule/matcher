63a02766d4ceaad6dee80450d0f6d17326c548fd
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-06 15:06:57 +0800

    match_pop

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,26 @@ function main() {
 }
 `;
 
-function main() {
-  console.log('你好，世界');
+function match_pop(matcher: string | RegExp, doc: string) {
+  if (matcher instanceof RegExp || matcher.startsWith('^')) {
+    const regex = new RegExp(matcher);
+    const result = regex.exec(doc);
+    if (result) return [matcher, result[0], doc.slice(result[0].length)];
+    return null;
+  } else {
+    if (doc.startsWith(matcher)) return [matcher, matcher, doc.slice(matcher.length)];
+    return null;
+  }
 }
+
+function parse(pattern: string, doc: string) {
+  const matchers = pattern.split(/\s+/);
+  console.log(match_pop(`function main()`, doc));
+}
+
+function main() {
+  // parse(regex, doc);
+  // console.log('123' instanceof RegExp);
+}
+
+main();
