fff8421a0f1b8f795065a4ee415bbc18bd784de3
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-06 16:01:30 +0800

    match_pop函数完善

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,35 @@
 
-const regex = `function $id ( ) { }`;
+const regex = `function main ( ) { }`;
 
 const doc = `
 function main() {
-  console.log('你好，世界');
+
 }
 `;
 
-function match_pop(matcher: string | RegExp, doc: string) {
+function
+match_pop(doc: string, matcher: string | RegExp) {
   if (matcher instanceof RegExp || matcher.startsWith('^')) {
     const regex = new RegExp(matcher);
     const result = regex.exec(doc);
-    if (result) return [matcher, result[0], doc.slice(result[0].length)];
-    return null;
-  } else {
-    if (doc.startsWith(matcher)) return [matcher, matcher, doc.slice(matcher.length)];
-    return null;
-  }
+    if (result) return { matcher, result: result[0], next: doc.slice(result[0].length) };
+  } else if (doc.startsWith(matcher))
+    return { matcher, result: matcher, next: doc.slice(matcher.length) };
+  throw { doc, matcher };
 }
 
 function parse(pattern: string, doc: string) {
+  const result: [string | RegExp, string][] = [];
   const matchers = pattern.split(/\s+/);
-  console.log(match_pop(`function main()`, doc));
+  doc = match_pop(doc, /\s*/).next;
+  matchers.forEach((matcher) => {
+    console.log(matcher);
+  });
+  return result;
 }
 
 function main() {
-  // parse(regex, doc);
-  // console.log('123' instanceof RegExp);
+  parse(regex, doc);
 }
 
 main();
